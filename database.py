2cae78de919ee569d9e1e37df4b6bc35785ac045
Rodiat Hammed <rodiathammed48@gmail.com>
2024-12-31 21:25:46 +0100

    fix typo: changed get_gb to get_db

--- database.py
+++ database.py
@@ -9,7 +9,7 @@ engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 Base = declarative_base()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-def get_gb():
+def get_db():
     db = SessionLocal()
     try:
         yield db
