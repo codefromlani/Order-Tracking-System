216b80b7ff602c6bfbfd1101b9ae34cba73c9328
Rodiat Hammed <rodiathammed48@gmail.com>
2025-01-13 21:04:40 +0100

    shipment mangement endpoints

---
+++ routers/shipping_router.py
@@ -0,0 +1,34 @@
+from fastapi import APIRouter, Depends, status
+from services import shipping_service
+from sqlalchemy.orm import Session
+from typing import List
+from database import get_db
+import schemas
+
+
+router = APIRouter(
+    tags=["Shipment"]
+)
+
+@router.get("/orders/{order_id}/shipment/dhl")
+async def get_dhl_tracking_status(
+    order_id: int, 
+    tracking_number: str,
+    db: Session = Depends(get_db)
+):
+    return shipping_service.get_dhl_tracking_status(order_id=order_id, tracking_number=tracking_number, db=db)
+
+@router.get("orders/{order_id}/shipment/status")
+async def get_shipment_status(
+    order_id: int, 
+    db: Session = Depends(get_db)
+):
+    return shipping_service.get_shipment_status(order_id=order_id, db=db)
+
+@router.patch("/shipment/{order_id}")
+async def update_shipment(
+    order_id: int, 
+    shipment_update: schemas.ShipmentUpdate, 
+    db: Session = Depends(get_db)
+):
+    return shipping_service.update_shipment(order_id=order_id, shipment_update=shipment_update, db=db)
