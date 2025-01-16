3d00dc598503b315f573e67990f3f79f9f7183f6
Rodiat Hammed <rodiathammed48@gmail.com>
2025-01-16 15:07:07 +0100

    shipment endpoints

--- routers/shipping_router.py
+++ routers/shipping_router.py
@@ -1,7 +1,6 @@
-from fastapi import APIRouter, Depends, status
-from services import shipping_service, DHL_service
+from fastapi import APIRouter, Depends
+from services import shipping_service
 from sqlalchemy.orm import Session
-from typing import List
 from database import get_db
 import schemas
 
@@ -10,30 +9,22 @@ router = APIRouter(
     tags=["Shipment"]
 )
 
-@router.post("/shipments/")
-async def create_shipment(
+@router.post("/orders/{order_id}/shipments/")
+def create_shipment(
     order_id: int,
     db: Session = Depends(get_db)
 ):
-    return await DHL_service.create_shipment(order_id=order_id, db=db)
+    return shipping_service.create_shipments(order_id=order_id, db=db)
 
-@router.get("/orders/{order_id}/shipment/dhl")
-async def get_dhl_tracking_status(
-    order_id: int, 
-    tracking_number: str,
-    db: Session = Depends(get_db)
-):
-    return shipping_service.get_dhl_tracking_status(order_id=order_id, tracking_number=tracking_number, db=db)
-
-@router.get("/orders/{order_id}/shipment/status")
-async def get_shipment_status(
+@router.get("/shipments/{order_id}/")
+def track_shipments(
     order_id: int, 
     db: Session = Depends(get_db)
 ):
-    return await shipping_service.get_shipment_status(order_id=order_id, db=db)
+    return shipping_service.track_shipments(order_id=order_id, db=db)
 
-@router.patch("/shipment/{order_id}")
-async def update_shipment(
+@router.patch("/shipments/{order_id}/update")
+def update_shipment(
     order_id: int, 
     shipment_update: schemas.ShipmentUpdate, 
     db: Session = Depends(get_db)
